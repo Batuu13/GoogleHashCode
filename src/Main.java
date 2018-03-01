f4b5bf8e924d865c37e3efa2fb48f0d400a95b04
Batuhan YAMAN <batu@schmiede.one>
2018-03-01 19:22:20 +0100

    parse and class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.util.LinkedList;
 
 public class Main {
-    public static void main(String [] args)
-    {
-        File file = new File("example.in");
+    public static LinkedList<Ride> rides = new LinkedList<>();
+    public static void main(String [] args) throws IOException {
+        File file = new File("a_example.in");
 
-        BufferedReader br = new BufferedReader(new FileReader(file));
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         String st;
+        st = br.readLine();
+        String [] firstLine = st.split(" ");
+        Integer rowCount = Integer.parseInt(firstLine[0]);
+        Integer colCount = Integer.parseInt(firstLine[1]);
+        Integer carCount = Integer.parseInt(firstLine[2]);
+        Integer rideCount = Integer.parseInt(firstLine[3]);
+        Integer bonus = Integer.parseInt(firstLine[4]);
+        Integer turn = Integer.parseInt(firstLine[5]);
+
         while ((st = br.readLine()) != null)
-            System.out.println(st);
+        {
+            String [] tempLine = st.split(" ");
+            rides.add(new Ride(tempLine));
+        }
     }
     }
-}
+
---
+++ src/Ride.java
@@ -0,0 +1,21 @@
+public class Ride {
+    public Integer x1;
+    public Integer x2;
+    public Integer y1;
+    public Integer y2;
+    public Integer startTime;
+    public Integer finishTime;
+
+    public Ride(String[] data){
+        this.x1 = Integer.parseInt(data[0]);
+        this.x2 = Integer.parseInt(data[1]);
+        this.y1 = Integer.parseInt(data[2]);
+        this.y2 = Integer.parseInt(data[3]);
+        this.startTime = Integer.parseInt(data[4]);
+        this.finishTime = Integer.parseInt(data[5]);
+    }
+
+    public Integer getDistance(Integer x,Integer y){
+        return Math.abs(x-this.x1) + Math.abs(y-this.y1);
+    }
+}
