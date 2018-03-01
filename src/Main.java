c86931143d385a4a445557ba7efa3564c4fbfc01
Ainuddin Faizan <andy@schmiede.one>
2018-03-01 20:15:18 +0100

    Added loop for intialising vehicles

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.LinkedList;
 
 public class Main {
     public static LinkedList<Ride> rides = new LinkedList<>();
+    public static LinkedList<Vehicle> vehicles = new LinkedList<>();
     public static void main(String [] args) throws IOException {
         File file = new File("a_example.in");
 
@@ -16,17 +17,21 @@ public class Main {
         String st;
         st = br.readLine();
         String [] firstLine = st.split(" ");
-        Integer rowCount = Integer.parseInt(firstLine[0]);
-        Integer colCount = Integer.parseInt(firstLine[1]);
-        Integer carCount = Integer.parseInt(firstLine[2]);
-        Integer rideCount = Integer.parseInt(firstLine[3]);
-        Integer bonus = Integer.parseInt(firstLine[4]);
-        Integer turn = Integer.parseInt(firstLine[5]);
+        int rowCount = Integer.parseInt(firstLine[0]);
+        int colCount = Integer.parseInt(firstLine[1]);
+        int vehicleCount = Integer.parseInt(firstLine[2]);
+        int rideCount = Integer.parseInt(firstLine[3]);
+        int bonus = Integer.parseInt(firstLine[4]);
+        int turn = Integer.parseInt(firstLine[5]);
 
         while ((st = br.readLine()) != null)
         {
             String [] tempLine = st.split(" ");
             rides.add(new Ride(tempLine));
+        }
+
+        for (int i = 0; i < vehicleCount; i++) {
+            vehicles.add(new Vehicle(new int[], 0, 0, true));
         }
     }
     }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,10 +2,12 @@ public class Vehicle {
     int[] rides;
     int currX;
     int currY;
+    boolean isAvailable;
 
-    public Vehicle(int[] rides, int currX, int currY) {
+    public Vehicle(int[] rides, int currX, int currY, boolean isAvailable) {
         this.rides = rides;
         this.currX = currX;
         this.currY = currY;
+        this.isAvailable = isAvailable;
     }
 }
